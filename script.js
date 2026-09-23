db0491e2e9e616ab078f985addd08a467cb287e4
Joey Briggs <joeyganash@gmail.com>
2026-09-23 18:14:16 +0300

    Add new book form and library display with styling

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const myLibrary = []
 
 function Book(title, author, pages, read) {
     if (!new.target) {
@@ -19,6 +18,38 @@ function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
-addBookToLibrary("The Hobbit", "Tolkien", "295 pages", "not read yet");
 
-console.log(myLibrary[0]);
+function displayLibrary(myLibrary) {
+    const library = document.querySelector(".library");
+    for (let book of myLibrary) {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        const bookTitle = document.createElement("h2")
+        bookTitle.textContent = book.title;
+        const bookAuthor = document.createElement("p");
+        bookAuthor.textContent = book.author;
+        const bookPages = document.createElement("p");
+        bookPages.textContent = book.pages;
+        const bookRead = document.createElement("p");
+        bookRead.textContent = book.read;
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bookPages);
+        bookCard.appendChild(bookRead);
+        library.appendChild(bookCard);
+    }
+}
+
+const myLibrary = []
+addBookToLibrary("The Hobbit", "Tolkien", "295 pages", "not read yet");
+addBookToLibrary("1984", "George Orwell", "368 pages", "not read yet");
+addBookToLibrary("The Very Hungry Caterpillar", "Eric Carle", "32 pages", "Have read")
+
+const newBookBtn = document.querySelector("#new-book-btn");
+const bookForm = document.querySelector("#book-form");
+
+newBookBtn.addEventListener("click", () => {
+    bookForm.hidden = false;
+});
+
+displayLibrary(myLibrary);
